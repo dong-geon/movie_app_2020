2774791c7eda457fb8ff53c471b0ec0dc54ff3b8
dong-geon <wge3142@gmail.com>
2020-07-09 00:11:48 +0900

    Updates

---
+++ precache-manifest.b5f7ede142320781bc67070f8ba2f7d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d57b681ea701e91ea89a8117fbd6c1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "29adcaf8e048be04b8ae",
+    "url": "/movie_app_2020/static/css/main.9728e8eb.chunk.css"
+  },
+  {
+    "revision": "825af1bb8907a10f974e",
+    "url": "/movie_app_2020/static/js/2.9e11718d.chunk.js"
+  },
+  {
+    "revision": "29adcaf8e048be04b8ae",
+    "url": "/movie_app_2020/static/js/main.8adc3a29.chunk.js"
+  },
+  {
+    "revision": "114d3434d006dc29083b",
+    "url": "/movie_app_2020/static/js/runtime~main.6c3fa8d1.js"
+  }
+]);
